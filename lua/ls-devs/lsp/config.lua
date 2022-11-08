a4f1772a0121c8656ef8fd926c7279db8e2e5847
ls-devs <58638832+ls-devs@users.noreply.github.com>
2022-11-08 17:28:30 +0100

    fix (lsp notify) : fix lsp no infos

--- lua/ls-devs/lsp/config.lua
+++ lua/ls-devs/lsp/config.lua
@@ -28,11 +28,21 @@ local config = {
 }
 
 vim.diagnostic.config(config)
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "rounded",
-})
-
+vim.lsp.handlers["textDocument/hover"] = function(_, result)
+	if not (result and result.contents) then
+		--vim.notify('No information available')
+		return
+	end
+	local markdown_lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
+	markdown_lines = vim.lsp.util.trim_empty_lines(markdown_lines)
+	if vim.tbl_isempty(markdown_lines) then
+		-- vim.notify('No information available')
+		return
+	end
+	return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", {
+		border = "rounded",
+	})
+end
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 	border = "rounded",
 })
