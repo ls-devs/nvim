c2bf67cef9486c59bf11b5b33452379526aad476
ls-devs <58638832+ls-devs@users.noreply.github.com>
2022-12-03 15:19:44 +0100

    fix(typo): rm blank line

--- lua/ls-devs/lsp/on_attach.lua
+++ lua/ls-devs/lsp/on_attach.lua
@@ -15,7 +15,6 @@ M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
 		client.server_capabilities.document_formatting = false
 	end
-
 	if client.name == "sumneko_lua" then
 		client.server_capabilities.document_formatting = false
 	end
