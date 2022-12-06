e123e99a854141812a1ca9bf2e25252a80cac906
ls-devs <58638832+ls-devs@users.noreply.github.com>
2022-12-06 08:26:18 +0100

    fix(rust-tools): update config

--- after/plugin/rust-tools.lua
+++ after/plugin/rust-tools.lua
@@ -28,7 +28,6 @@ rt.setup({
 			keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 			keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 			keymap(bufnr, "n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-			keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 			keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 			keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
 			keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
--- lua/ls-devs/lsp/on_attach.lua
+++ lua/ls-devs/lsp/on_attach.lua
@@ -18,9 +18,9 @@ M.on_attach = function(client, bufnr)
 	if client.name == "sumneko_lua" then
 		client.server_capabilities.document_formatting = false
 	end
-	if client.name == "rust_analyser" then
-		client.server_capabilities.document_formatting = false
-	end
+	--[[ if client.name == "rust_analyser" then ]]
+	--[[ 	client.server_capabilities.document_formatting = false ]]
+	--[[ end ]]
 	if client.name == "pyright" then
 		client.server_capabilities.document_formatting = false
 	end
