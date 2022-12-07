d3dc441f7ba10e292bede65d5e11d1609b6ca203
ls-devs <58638832+ls-devs@users.noreply.github.com>
2022-12-07 10:40:57 +0100

    feat(dap) config cpp debugger

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -15,3 +15,31 @@ dap.configurations.python = {
 		end,
 	},
 }
+
+dap.adapters.cpp = {
+	type = "executable",
+	attach = {
+		pidProperty = "pid",
+		pidSelect = "ask",
+	},
+	command = "lldb-vscode-15", -- my binary was called 'lldb-vscode-11'
+	env = {
+		LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
+	},
+	name = "lldb",
+}
+
+dap.configurations.cpp = {
+	{
+		name = "lldb",
+		type = "cpp",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		externalTerminal = false,
+		stopOnEntry = false,
+		args = {},
+	},
+}
