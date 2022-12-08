2d67a864ff4781809d659fa8474622e735b04fec
ls-devs <58638832+ls-devs@users.noreply.github.com>
2022-12-08 15:15:18 +0100

    feat(dap-cpp): add dap cpp, use for rust too

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,9 +1,20 @@
+local codelldb_path = vim.env.HOME .. "/.vscode-insiders/extensions/vadimcn.vscode-lldb-1.8.1/adapter/codelldb"
 local dap = require("dap")
 dap.adapters.python = {
 	type = "executable",
 	command = "python3.10",
 	args = { "-m", "debugpy.adapter" },
 }
+
+dap.adapters.codelldb = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = codelldb_path,
+		args = { "--port", "${port}" },
+	},
+}
+
 dap.configurations.python = {
 	{
 		type = "python",
@@ -16,30 +27,19 @@ dap.configurations.python = {
 	},
 }
 
-dap.adapters.cpp = {
-	type = "executable",
-	attach = {
-		pidProperty = "pid",
-		pidSelect = "ask",
-	},
-	command = "lldb-vscode-15", -- my binary was called 'lldb-vscode-11'
-	env = {
-		LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES",
-	},
-	name = "lldb",
-}
-
 dap.configurations.cpp = {
 	{
-		name = "lldb",
-		type = "cpp",
+		name = "Launch file",
+
+		type = "codelldb",
 		request = "launch",
 		program = function()
 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 		end,
 		cwd = "${workspaceFolder}",
-		externalTerminal = false,
 		stopOnEntry = false,
-		args = {},
 	},
 }
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
