0ff0b52b83cd6d8b72e3086bef8602268e374aca
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-01-04 13:50:09 +0100

    feat(signature): update signature

--- after/plugin/lsp-signature.lua
+++ after/plugin/lsp-signature.lua
@@ -11,22 +11,21 @@ local cfg = {
 	-- This setting only take effect in insert mode, it does not affect signature help in normal
 	-- mode, 10 by default
 
-	max_height = 12, -- max height of signature floating_window
-	max_width = 80, -- max_width of signature floating_window
 	noice = false, -- set to true if you using noice to render markdown
 	wrap = true, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
-
-	floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+  max_height = 12, -- max height of signature floating_window
+  max_width = 50, -- max_width of signature floating_window
+	floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
 	floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
 	-- will set to true when fully tested, set to false will use whichever side has more space
 	-- this setting will be helpful if you do not want the PUM and floating win overlap
 
-	floating_window_off_x = 5, -- adjust float windows x position.
+	floating_window_off_x = 0, -- adjust float windows x position.
 	floating_window_off_y = 0, -- adjust float windows y position. e.g -2 move window up 2 lines; 2 move down 2 lines
 	-- can be either number or function, see examples
 
-	close_timeout = 4000, -- close floating window after ms when laster parameter is entered
+	close_timeout = 3000, -- close floating window after ms when laster parameter is entered
 	fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
 	hint_enable = true, -- virtual hint enable
 	hint_prefix = "🐭 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
@@ -45,11 +44,11 @@ local cfg = {
 	padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
 
 	transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-	timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
+	timer_interval = 10, -- default timer check interval set to lower value if you want to reduce latency
 	toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 
 	select_signature_key = nil, -- cycle to next signature, e.g. '<M-n>' function overloading
-	move_cursor_key = nil, -- imap, use nvim_set_current_win to move cursor between current win and floating
+	move_cursor_key = "<C-k>", -- imap, use nvim_set_current_win to move cursor between current win and floating
 }
 
 -- recommended:
