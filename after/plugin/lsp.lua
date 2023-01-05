814fd0063d8032f5179e96be8209d2a51f91960a
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-01-05 21:24:25 +0100

    feat(lsp-zero): lsp-zero working

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,7 +7,6 @@ require("mason.settings").set({
 require("lspconfig.ui.windows").default_options.border = "rounded"
 
 local lsp = require("lsp-zero")
-lsp.preset("recommended")
 
 local luasnip = require("luasnip")
 
@@ -15,57 +14,29 @@ local lspkind = require("lspkind")
 
 local luasnip_vscode_loader = require("luasnip/loaders/from_vscode")
 
+local cmp = require("cmp")
+
+lsp.preset("recommended")
+
 luasnip_vscode_loader.lazy_load()
+
+cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+})
 
 local check_backspace = function()
 	local col = vim.fn.col(".") - 1
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
-local cmp = require("cmp")
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-k>"] = cmp.mapping.select_prev_item(),
-	["<C-j>"] = cmp.mapping.select_next_item(),
-	["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-	["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-	["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-	["<C-y>"] = cmp.config.disable,
-	["<C-e>"] = cmp.mapping({
-		i = cmp.mapping.abort(),
-		c = cmp.mapping.close(),
-	}),
-	["<CR>"] = cmp.mapping.confirm({ select = true }),
-	["<Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_next_item()
-		elseif luasnip.expandable() then
-			luasnip.expand()
-		elseif luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		elseif check_backspace() then
-			fallback()
-		else
-			fallback()
-		end
-	end, {
-		"i",
-		"s",
-	}),
-	["<S-Tab>"] = cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_prev_item()
-		elseif luasnip.jumpable(-1) then
-			luasnip.jump(-1)
-		else
-			fallback()
-		end
-	end, {
-		"i",
-		"s",
-	}),
-})
-
 lsp.setup_nvim_cmp({
+	preselect = require("cmp").PreselectMode.None,
+	completion = {
+		completeopt = "menu,menuone,noinsert,noselect",
+	},
 	sorting = {
 		comparators = {
 			cmp.config.compare.offset,
@@ -144,14 +115,34 @@ lsp.setup_nvim_cmp({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
 	experimental = {
 		ghost_text = false,
 		native_menu = false,
 	},
+})
+
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
 
 local signs = {
@@ -186,6 +177,7 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 local get_servers = require("mason-lspconfig").get_installed_servers
+
 for _, server in pairs(get_servers()) do
 	local config_exists, config = pcall(require, "ls-devs.lsp.settings." .. server)
 
@@ -196,4 +188,10 @@ end
 
 lsp.nvim_workspace()
 
+lsp.skip_server_setup({ "rust_analyzer" })
+
 lsp.setup()
+
+local rust_lsp = lsp.build_options("rust_analyzer", {})
+
+require("rust-tools").setup({ server = rust_lsp })
