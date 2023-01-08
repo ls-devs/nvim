594ce692d88dfb325025ccf15db2f5b492a7699b
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-01-08 21:20:11 +0100

    feat(lazy): add more lazy load WIP

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -1,9 +1,5 @@
 local autopairs = require("nvim-autopairs")
 
-if not autopairs then
-	return
-end
-
 autopairs.setup({
 	check_ts = true,
 	ts_config = {
@@ -27,14 +23,6 @@ autopairs.setup({
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-if not cmp_autopairs then
-	return
-end
-
 local cmp = require("cmp")
-
-if not cmp then
-	return
-end
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
