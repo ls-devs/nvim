c2779caefb1a69bc85096eee4aef7e9c058ef807
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-01-09 17:59:19 +0100

    fix(mason-null-ls): fix config file, respect semantic

--- lua/ls-devs/lazy/mason-null-ls.lua
+++ lua/ls-devs/lazy/mason-null-ls.lua
@@ -1,30 +1,36 @@
-return function()
-	local mason_null_ls = require("mason-null-ls")
-	if not mason_null_ls then
-		return
-	end
+local M = {}
 
-	mason_null_ls.setup({
-		automatic_installation = true,
-		ensure_installed = {
-			"black",
-			"stylua",
-			"luacheck",
-			"eslint_d",
-			"prettierd",
-			"rustfmt",
-			"prismaFmt",
-			"cmakelang",
-			"markdownlint",
-			"yamllint",
-			"yamlfmt",
-			"jsonlint",
-			"jq",
-			"phpcs",
-			"php-cs-fixer",
-			"proselint",
-			"refactoring",
-			"hadolint",
-		},
-	})
+M.config = function()
+  return function()
+    local mason_null_ls = require("mason-null-ls")
+    if not mason_null_ls then
+      return
+    end
+
+    mason_null_ls.setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "black",
+        "stylua",
+        "luacheck",
+        "eslint_d",
+        "prettierd",
+        "rustfmt",
+        "prismaFmt",
+        "cmakelang",
+        "markdownlint",
+        "yamllint",
+        "yamlfmt",
+        "jsonlint",
+        "jq",
+        "phpcs",
+        "php-cs-fixer",
+        "proselint",
+        "refactoring",
+        "hadolint",
+      },
+    })
+  end
 end
+
+return M
