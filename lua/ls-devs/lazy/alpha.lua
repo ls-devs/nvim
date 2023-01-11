5350646cf66311999b449f6ad20f5576004629e7
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-01-11 23:31:41 +0100

    fix(alpha): fix statusline flickering

--- lua/ls-devs/lazy/alpha.lua
+++ lua/ls-devs/lazy/alpha.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.config = function()
-  vim.api.nvim_exec([[autocmd FileType alpha set laststatus=0]], false)
+  vim.api.nvim_exec([[autocmd FileType alpha set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3]], false)
   local dashboard = require("alpha.themes.dashboard")
   local version = vim.version()
   local lazy = require("lazy")
