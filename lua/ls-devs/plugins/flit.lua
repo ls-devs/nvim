f95394eacea0a55c5761d87e4b6349fe9a93322a
ls-devs <58638832-ls-devs@users.noreply.github.com>
2023-02-22 15:01:29 +0100

    feat(flit): add flit, remove leap & hop

--- lua/ls-devs/plugins/flit.lua
+++ lua/ls-devs/plugins/flit.lua
@@ -4,7 +4,7 @@ M.config = function()
   require("flit").setup({
     keys = { f = "f", F = "F", t = "t", T = "T" },
     -- A string like "nv", "nvo", "o", etc.
-    labeled_modes = "v",
+    labeled_modes = "nvo",
     multiline = true,
     -- Like `leap`s similar argument (call-specific overrides).
     -- E.g.: opts = { equivalence_classes = {} }
