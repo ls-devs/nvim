9b61cd202d680b1269b7f935b78a10de2bf5eda6
ls-devs <58638832-ls-devs@users.noreply.github.com>
2023-02-23 12:46:57 +0100

    fix(dapui): fix dapui do not open on event

--- lua/ls-devs/plugins/dapui.lua
+++ lua/ls-devs/plugins/dapui.lua
@@ -1,26 +1,15 @@
 local M = {}
 
 M.config = function()
-  local dap, dapui = require("dap"), require("dapui")
-  dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-  end
-  dap.listeners.after.event_terminated["dapui_config"] = function()
-    dapui.close()
-  end
-  dap.listeners.after.event_exited["dapui_config"] = function()
-    dapui.close()
-  end
-
-  dapui.setup()
+  require("dapui").setup()
 end
 
 M.keys = {
   { "<leader>uo", "<cmd>lua require('dapui').toggle()<CR>", desc = "DapUI Toggle" },
-  { "<leader>uc", "<cmd>lua require('dapui').close()<CR>", desc = "DapUI Close" },
-  { "<leader>un", ":DapContinue<CR>", desc = "DapContinue" },
-  { "<leader>ut", ":DapTerminate<CR>", desc = "DapTerminate" },
-  { "<leader>bb", ":DapToggleBreakpoint<CR>", desc = "DapToggleBreakpoint" }
+  { "<leader>uc", "<cmd>lua require('dapui').close()<CR>",  desc = "DapUI Close" },
+  { "<leader>un", ":DapContinue<CR>",                       desc = "DapContinue" },
+  { "<leader>ut", ":DapTerminate<CR>",                      desc = "DapTerminate" },
+  { "<leader>bb", ":DapToggleBreakpoint<CR>",               desc = "DapToggleBreakpoint" },
 }
 
 return M
