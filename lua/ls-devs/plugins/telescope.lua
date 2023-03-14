f1ccda8d90e464cd39fbeb409138bf7746721981
ls-devs <58638832-ls-devs@users.noreply.github.com>
2023-03-14 12:27:38 +0100

    feat(plugin): add telescope media_file, leap & leap-spooky config

--- lua/ls-devs/plugins/telescope.lua
+++ lua/ls-devs/plugins/telescope.lua
@@ -1,44 +1,37 @@
 return function()
-	local telescope = require("telescope")
-	local actions = require("telescope.actions")
+  local telescope = require("telescope")
+  local actions = require("telescope.actions")
 
-	if not telescope then
-		return
-	end
-
-	if not actions then
-		return
-	end
-
-	telescope.setup({
-		defaults = {
-			file_ignore_patterns = {
-				".git/",
-				"node_modules/*",
-			},
-			mappings = {
-				i = {
-					["<C-j>"] = actions.move_selection_next,
-					["<C-k>"] = actions.move_selection_previous,
-					["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-					["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				},
-				n = {
-					["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-					["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-				},
-			},
-		},
-		pickers = {
-			find_files = {
-				theme = "dropdown",
-				previewer = false,
-			},
-			buffers = {
-				theme = "dropdown",
-				previewer = false,
-			},
-		},
-	})
-	telescope.load_extension("fzf")
+  telescope.setup({
+    defaults = {
+      file_ignore_patterns = {
+        ".git/",
+        "node_modules/*",
+      },
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+          ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        },
+        n = {
+          ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+          ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        },
+      },
+    },
+    pickers = {
+      find_files = {
+        theme = "dropdown",
+        previewer = false,
+      },
+      buffers = {
+        theme = "dropdown",
+        previewer = false,
+      },
+    },
+  })
+  telescope.load_extension("fzf")
+  telescope.load_extension("media_files")
 end
