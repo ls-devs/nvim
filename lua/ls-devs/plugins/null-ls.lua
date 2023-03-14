bc153364d131bdbe27057e54e9f65c992c87472c
ls-devs <58638832-ls-devs@users.noreply.github.com>
2023-03-14 12:25:47 +0100

    feat(rustfmt): check for edition (cargo.toml or default)

--- lua/ls-devs/plugins/null-ls.lua
+++ lua/ls-devs/plugins/null-ls.lua
@@ -33,7 +33,21 @@ M.config = function()
       null_ls.builtins.formatting.prismaFmt,
       -- RUST
       null_ls.builtins.formatting.rustfmt.with({
-        filetypes = { "rust" },
+        extra_args = function(params)
+          local Path = require("plenary.path")
+          local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
+
+          if cargo_toml:exists() and cargo_toml:is_file() then
+            for _, line in ipairs(cargo_toml:readlines()) do
+              local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+              if edition then
+                return { "--edition=" .. edition }
+              end
+            end
+          end
+          -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+          return { "--edition=2021" }
+        end,
       }),
       -- CMAKE
       null_ls.builtins.diagnostics.cmake_lint,
