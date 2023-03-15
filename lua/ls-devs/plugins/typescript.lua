5899d31ef0bb85c8ef2a3b52e4ffd9776db4438d
ls-devs <58638832-ls-devs@users.noreply.github.com>
2023-03-15 19:04:35 +0100

    feat(typescript) : config typescript plugin + config lsp_zero & volar

--- lua/ls-devs/plugins/typescript.lua
+++ lua/ls-devs/plugins/typescript.lua
@@ -1,21 +1,12 @@
 local M = {}
 
 M.config = function()
-  local lsp = require("lsp-zero")
-
-  local tsserver_opts = lsp.build_options("tsserver", {
-    on_attach = function(client, bufnr)
-      client.server_capabilities.document_formatting = false
-      vim.keymap.set("n", "<leader>ci", "<cmd>TypescriptOrganizeImports<cr>", { buffer = bufnr })
-    end,
-  })
   require("typescript").setup({
     disable_commands = false, -- prevent the plugin from creating Vim commands
     debug = false,          -- enable debug logging for commands
     go_to_source_definition = {
       fallback = true,      -- fall back to standard LSP definition on failure
     },
-    server = tsserver_opts,
   })
 end
 
