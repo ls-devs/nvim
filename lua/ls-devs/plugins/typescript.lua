29fffe6288c5066e971975662739cf805dde12b7
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-07-11 17:25:34 +0200

    fix: prettier_d_slim
    Not working atm

--- lua/ls-devs/plugins/typescript.lua
+++ lua/ls-devs/plugins/typescript.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = function()
   require("typescript").setup({
     disable_commands = false, -- prevent the plugin from creating Vim commands
-    debug = true,          -- enable debug logging for commands
+    debug = false,          -- enable debug logging for commands
     go_to_source_definition = {
       fallback = true,      -- fall back to standard LSP definition on failure
     },
