78d82a48a392ec7928778de82cf4518cf5ab7eef
ls-devs <58638832+ls-devs@users.noreply.github.com>
2023-10-19 17:08:58 +0200

    fix: lsp-timeout restart

--- lua/ls-devs/plugins/lsp-timeout.lua
+++ lua/ls-devs/plugins/lsp-timeout.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = function()
   vim.g["lsp-timeout-config"] = {
     stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSP servers
-    startTimeout = 1000 * 10,  -- ms, timeout before restart
+    startTimeout = 1000,  -- ms, timeout before restart
     silent = false,            -- true to suppress notifications
   }
 end
