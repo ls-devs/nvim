db2f9f458b38ec075e4f1afd8d1e289193363f5b
Laurent <laurent.seigneurie@soprasteria.com>
2024-01-17 22:34:47 +0100

    fix: add quit terminal keymap

--- lua/ls-devs/plugins/toggleterm.lua
+++ lua/ls-devs/plugins/toggleterm.lua
@@ -37,6 +37,7 @@ M.config = function()
 
   function _G.set_terminal_keymaps()
     local opts = { noremap = true }
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-x>", [[<Cmd>q!<CR>]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
     vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
