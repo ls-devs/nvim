d538aac4e07fbdadf8c91ed51cc6471d3d6d3db1
Laurent <laurent.seigneurie@soprasteria.com>
2024-01-22 15:24:23 +0100

    feat: overseer / timeout

--- lua/ls-devs/plugins/lsp-timeout.lua
+++ lua/ls-devs/plugins/lsp-timeout.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.config = function()
   vim.g.lspTimeoutConfig = {
-    stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSP servers
+    stopTimeout = 1000 * 60 * 7, -- ms, timeout before stopping all LSP servers
     startTimeout = 500,        -- ms, timeout before restart
     silent = false,            -- true to suppress notifications
   }
