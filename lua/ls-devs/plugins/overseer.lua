eed3d8aa7579e3dd59ca57111cdee81de29f4e9b
Laurent <laurent.seigneurie@soprasteria.com>
2024-01-23 16:41:22 +0100

    chore: formatting

--- lua/ls-devs/plugins/overseer.lua
+++ lua/ls-devs/plugins/overseer.lua
@@ -1,66 +1,66 @@
 local M = {}
 
 M.config = function()
-	require("overseer").setup({
-		form = {
-			win_opts = {
-				winblend = 0,
-			},
-		},
-		confirm = {
-			win_opts = {
-				winblend = 0,
-			},
-		},
-		task_win = {
-			win_opts = {
-				winblend = 0,
-			},
-		},
-		task_list = {
-			bindings = {
-				["?"] = "ShowHelp",
-				["g?"] = "ShowHelp",
-				["<CR>"] = "RunAction",
-				["<C-e>"] = "Edit",
-				["o"] = "Open",
-				["<C-v>"] = "OpenVsplit",
-				["<C-s>"] = "OpenSplit",
-				["<C-f>"] = "OpenFloat",
-				["<C-q>"] = "OpenQuickFix",
-				["p"] = "TogglePreview",
-				["<C-o>"] = "IncreaseDetail",
-				["<C-c>"] = "DecreaseDetail",
-				["L"] = "IncreaseAllDetail",
-				["H"] = "DecreaseAllDetail",
-				["["] = "DecreaseWidth",
-				["]"] = "IncreaseWidth",
-				["{"] = "PrevTask",
-				["}"] = "NextTask",
-				["<C-k>"] = "ScrollOutputUp",
-				["<C-j>"] = "ScrollOutputDown",
-			},
-		},
-	})
+  require("overseer").setup({
+    form = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    confirm = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    task_win = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    task_list = {
+      bindings = {
+        ["?"] = "ShowHelp",
+        ["g?"] = "ShowHelp",
+        ["<CR>"] = "RunAction",
+        ["<C-e>"] = "Edit",
+        ["o"] = "Open",
+        ["<C-v>"] = "OpenVsplit",
+        ["<C-s>"] = "OpenSplit",
+        ["<C-f>"] = "OpenFloat",
+        ["<C-q>"] = "OpenQuickFix",
+        ["p"] = "TogglePreview",
+        ["<C-o>"] = "IncreaseDetail",
+        ["<C-c>"] = "DecreaseDetail",
+        ["L"] = "IncreaseAllDetail",
+        ["H"] = "DecreaseAllDetail",
+        ["["] = "DecreaseWidth",
+        ["]"] = "IncreaseWidth",
+        ["{"] = "PrevTask",
+        ["}"] = "NextTask",
+        ["<C-k>"] = "ScrollOutputUp",
+        ["<C-j>"] = "ScrollOutputDown",
+      },
+    },
+  })
 end
 
 -- Fix Overseer with Windows
 local isOpen = false
 function OverseerToggle()
-	if not isOpen then
-		vim.cmd(":OverseerToggle")
-		vim.cmd(":WindowsDisableAutowidth")
-		isOpen = true
-	else
-		vim.cmd(":OverseerToggle")
-		vim.cmd(":WindowsEnableAutowidth")
-		isOpen = false
-	end
+  if not isOpen then
+    vim.cmd(":OverseerToggle")
+    vim.cmd(":WindowsDisableAutowidth")
+    isOpen = true
+  else
+    vim.cmd(":OverseerToggle")
+    vim.cmd(":WindowsEnableAutowidth")
+    isOpen = false
+  end
 end
 
 M.keys = {
-	{ "<leader>or", "<cmd>OverseerRun<CR>", desc = "Overseer Run" },
-	{ "<leader>ot", "<cmd>lua OverseerToggle()<CR>", desc = "Overseer Toggle" },
+  { "<leader>or", "<cmd>OverseerRun<CR>",          desc = "Overseer Run" },
+  { "<leader>ot", "<cmd>lua OverseerToggle()<CR>", desc = "Overseer Toggle" },
 }
 
 return M
