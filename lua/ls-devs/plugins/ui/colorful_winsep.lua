c50695f1380c1c2b883ef47cf61e62788514b932
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2024-02-24 12:17:06 +0100

    [FEAT]: Colorful-Winsep config

--- lua/ls-devs/plugins/ui/colorful_winsep.lua
+++ lua/ls-devs/plugins/ui/colorful_winsep.lua
@@ -6,26 +6,8 @@ return {
 		no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree", "OverseerList" },
 		symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
 		close_event = function() end,
+		hi = {
+			fg = "#f5e0dc",
+		},
 	},
-	config = function(_, opts)
-		local colors = require("catppuccin.palettes.mocha")
-		require("colorful-winsep").setup(vim.tbl_deep_extend("force", opts, {
-			hi = {
-				fg = colors.peach,
-			},
-			create_event = function()
-				local win_n = require("colorful-winsep.utils").calculate_number_windows()
-				if win_n == 2 then
-					local win_id = vim.fn.win_getid(vim.fn.winnr("h"))
-					if win_id then
-						local filetype =
-							vim.api.nvim_get_option_value("filetype", { buf = vim.api.nvim_win_get_buf(win_id) })
-						if filetype == "NvimTree" then
-							require("colorful_winsep").NvimSeparatorDel()
-						end
-					end
-				end
-			end,
-		}))
-	end,
 }
