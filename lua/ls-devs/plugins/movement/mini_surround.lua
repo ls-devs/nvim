b17d2cae80bc07bbdcbb566d82f93d4f1db7bd53
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2024-03-03 11:54:33 +0100

    [FEAT]: Add sleuth
    Automatically set tabstop and shiftwidth

--- lua/ls-devs/plugins/movement/mini_surround.lua
+++ lua/ls-devs/plugins/movement/mini_surround.lua
@@ -2,13 +2,13 @@ return {
 	"echasnovski/mini.surround",
 	opts = {
 		mappings = {
-			add = "gza", -- Add surrounding in Normal and Visual modes
-			delete = "gzd", -- Delete surrounding
-			find = "gzf", -- Find surrounding (to the right)
-			find_left = "gzF", -- Find surrounding (to the left)
-			highlight = "gzh", -- Highlight surrounding
-			replace = "gzr", -- Replace surrounding
-			update_n_lines = "gzn", -- Update `n_lines`
+			add = "gza",
+			delete = "gzd",
+			find = "gzf",
+			find_left = "gzF",
+			highlight = "gzh",
+			replace = "gzr",
+			update_n_lines = "gzn",
 		},
 	},
 	keys = { { "gz", mode = { "n", "v" }, desc = "Surround", noremap = true, silent = true } },
