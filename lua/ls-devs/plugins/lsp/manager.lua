148da405db9b97fca07c9255d102f69d96d700e9
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2024-03-06 09:06:01 +0100

    [FEAT]: Add somesass_ls

--- lua/ls-devs/plugins/lsp/manager.lua
+++ lua/ls-devs/plugins/lsp/manager.lua
@@ -344,6 +344,14 @@ return {
 							require("cmp_nvim_lsp").default_capabilities()
 						),
 					})
+					require("lspconfig")["somesass_ls"].setup({
+						capabilities = vim.tbl_deep_extend(
+							"force",
+							{},
+							vim.lsp.protocol.make_client_capabilities(),
+							require("cmp_nvim_lsp").default_capabilities()
+						),
+					})
 				end,
 			},
 		},
