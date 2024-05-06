6276f46ed45e1e9acfbf73cbbcb535fe305b2487
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2024-05-06 10:19:37 +0200

    [FEAT]: Avoid "No information available" messages

--- lua/ls-devs/plugins/ui/noice.lua
+++ lua/ls-devs/plugins/ui/noice.lua
@@ -75,6 +75,15 @@ return {
 			replace = true,
 			merge = true,
 		},
+		routes = {
+			{
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+			},
+		},
 		messages = {
 			enabled = true,
 		},
