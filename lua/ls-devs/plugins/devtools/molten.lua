e39710ac1a25849adbe792db56e065ae96331813
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2024-09-19 14:49:39 +0200

    [FRAT]: Molten conf

--- lua/ls-devs/plugins/devtools/molten.lua
+++ lua/ls-devs/plugins/devtools/molten.lua
@@ -123,6 +123,8 @@ return {
 		init = function()
 			vim.g.molten_image_provider = "image.nvim"
 			vim.g.molten_output_win_max_height = 20
+			vim.g.molten_virt_text_output = true
+			vim.g.molten_auto_open_output = false
 		end,
 	},
 	{
