5ca3fd8a55189ec1dfae63509bdb4f84ebf11f87
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2025-07-08 14:17:27 +0200

    [FIX]: Origami no compatible with ufo

--- lua/ls-devs/plugins/utilities/origami.lua
+++ lua/ls-devs/plugins/utilities/origami.lua
@@ -1,6 +1,7 @@
 return {
 	"chrisgrieser/nvim-origami",
 	event = { "BufReadPost", "BufNewFile" }, -- later or on keypress would prevent saving folds
+	version = "v1.9",
 	opts = {
 		keepFoldsAcrossSessions = false,
 		pauseFoldsOnSearch = true,
