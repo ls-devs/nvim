337330b897ce09411098fb4883bd7802faafa826
Seigneurie Laurent <laurent.seigneurie@soprasteria.com>
2025-09-24 10:50:27 +0200

    [FEAT]: Origami Last version

--- lua/ls-devs/plugins/utilities/origami.lua
+++ lua/ls-devs/plugins/utilities/origami.lua
@@ -1,7 +1,6 @@
 return {
 	"chrisgrieser/nvim-origami",
 	event = { "BufReadPost", "BufNewFile" }, -- later or on keypress would prevent saving folds
-	version = "v1.9",
 	opts = {
 		keepFoldsAcrossSessions = false,
 		pauseFoldsOnSearch = true,
